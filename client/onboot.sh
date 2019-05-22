7b68cbecb9be1d716551cfee5c7aa5d7c09018ee
sainate <nathan@piratica.us>
2019-05-22 19:50:55 -0400

    Added client/onboot.sh and took some of the functionality out of client/provision.sh and put it into client/onboot.sh

--- client/onboot.sh
+++ client/onboot.sh
@@ -13,27 +13,28 @@ source config.sh
 # Setup our SSH Tunnel
 $SUDO -u $C2USER ssh -p $SSHPORT -R $PROXYPORT:localhost:22 $C2 -C -i /home/$C2USER/.ssh/id_ecdsa -N -f
 if [ -z $? = 1 ]; then
-	MSG .= "Failed to establish SSH  Tunnel"
-fi
+		MSG .= "Failed to establish SSH  Tunnel"
+	fi
 
-$SSH -p $SSHPORT -L 2525:localhost:25 $C2USER@$C2 -N -f -C
-if [ -z $? = 1 ]; then
-	MSG .= "Failed to establish SMTP Tunnel"
-fi
+	$SSH -p $SSHPORT -L 2525:localhost:25 $C2USER@$C2 -N -f -C
+	if [ -z $? = 1 ]; then
+			MSG .= "Failed to establish SMTP Tunnel"
+		fi
 
 
-WAN_IP=$(curl ifconfig.me)
-SERIAL=$(dmidecode -s system-serial-number)
-UPTIME=$(uptime)
+		WAN_IP=$(curl ifconfig.me)
+		SERIAL=$(dmidecode -s system-serial-number)
+		UPTIME=$(uptime)
 
-echo "LAN IP = $LAN_IP " > $LOKIHOME/netinfo
-echo "WAN IP = $WAN_IP" >> $LOKIHOME/netinfo
-echo "SSH Proxy Port = $PROXYPORT" >> $LOKIHOME/netinfo 
-echo $MSG >>  $LOKIHOME/netinfo
+		echo "LAN IP = $LAN_IP " > $LOKIHOME/netinfo
+		echo "WAN IP = $WAN_IP" >> $LOKIHOME/netinfo
+		echo "SSH Proxy Port = $PROXYPORT" >> $LOKIHOME/netinfo 
+		echo $MSG >>  $LOKIHOME/netinfo
 
-echo "===========================" >> $LOKIHOME/netinfo
-echo "Serial = $SERIAL" >> $LOKIHOME/netinfo
-echo "MAC = $MAC" >> $LOKIHOME/netinfo
-echo "Uptime = $UPTIME" >> $LOKIHOME/netinfo
+		echo "===========================" >> $LOKIHOME/netinfo
+		echo "Serial = $SERIAL" >> $LOKIHOME/netinfo
+		echo "MAC = $MAC" >> $LOKIHOME/netinfo
+		echo "Uptime = $UPTIME" >> $LOKIHOME/netinfo
 
-mail -s "PSA ONLINE" $DESTEMAIL < $LOKIHOME/netinfo
+		mail -s "PSA ONLINE" $DESTEMAIL < $LOKIHOME/netinfo
+
